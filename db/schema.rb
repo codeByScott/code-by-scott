67b9b9d45c95cfd42dbf71ae7db7cca78b4d5b36
codeByScott <scott@codebyscott.io>
2017-06-30 16:03:33 +0900

    add user/post association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630042529) do
+ActiveRecord::Schema.define(version: 20170630065048) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at",         null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170630042529) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
